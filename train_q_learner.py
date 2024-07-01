08b5eafcddd63a5aa07af4941c7d4f4012aeb652
jmmckechnie <jmmckechnie@gmail.com>
2024-07-01 23:54:20 +0100

    Add docstring

--- train_q_learner.py
+++ train_q_learner.py
@@ -21,9 +21,12 @@ from environment import Environment
 from Robots.q_learning_robot import QLearningRobot
 
 
-def get_trained_q_learning_robot(name, number_of_actions=200, number_of_episodes=400, 
-                                 epsilon=0.99, decay_factor=0.99, learning_rate=0.1, 
+def get_trained_q_learning_robot(name, number_of_actions=200, number_of_episodes=400,
+                                 epsilon=0.99, decay_factor=0.99, learning_rate=0.1,
                                  discount_factor=0.9):
+    """
+    Implement the Q-Learning algorithm above with respect to our QLearningRobot.
+    """
     seed(42)
     learning_robot = QLearningRobot(name,
                                     epsilon=epsilon,
