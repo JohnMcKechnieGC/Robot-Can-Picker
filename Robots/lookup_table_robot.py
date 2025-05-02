fc2bffba4473f1af964e05059f4744a46499fc5a
jmmckechnie <jmmckechnie@gmail.com>
2025-05-02 14:32:46 +0100

    remove the need to subtract 1 from lookup table robot actions

--- Robots/lookup_table_robot.py
+++ Robots/lookup_table_robot.py
@@ -5,16 +5,21 @@ class LookupTableRobot(BaseRobot):
     def __init__(self, name='', action_lookup_table=None):
         super().__init__(name)
         if action_lookup_table is None:
-            # This default lookup table is the result of evolving the robot controller using a genetic algorithm.
+            # This default lookup table is the result of evolving the
+            # * robot controller using a genetic algorithm.
             action_lookup_table =\
-                [2, 2, 5, 4, 4, 4, 5, 5, 5, 1, 1, 1, 1, 1, 2, 1, 1, 1, 2, 6, 6, 6, 6, 2, 2, 6, 4, 5, 5, 5, 5, 5, 1, 3,
-                 4, 5, 1, 1, 1, 5, 1, 6, 6, 1, 1, 6, 6, 2, 1, 4, 4, 2, 2, 3, 3, 6, 3, 4, 2, 2, 5, 7, 5, 1, 3, 1, 1, 4,
-                 7, 4, 2, 2, 6, 4, 4, 3, 2, 3, 2, 4, 4, 3, 5, 5, 5, 2, 6, 5, 3, 5, 3, 3, 1, 1, 1, 3, 1, 1, 1, 4, 6, 2,
-                 7, 3, 1, 6, 4, 5, 2, 2, 5, 5, 1, 5, 3, 1, 5, 5, 1, 1, 3, 5, 6, 3, 3, 1, 3, 2, 2, 5, 4, 6, 7, 4, 2, 3,
-                 6, 3, 3, 6, 6, 6, 5, 6, 1, 6, 1, 1, 1, 2, 1, 6, 6, 2, 1, 3, 7, 6, 2, 2, 6, 6, 4, 2, 5, 4, 2, 1, 2, 2,
-                 4, 1, 1, 6, 1, 1, 4, 1, 1, 5, 1, 4, 4, 4, 4, 1, 4, 4, 2, 4, 1, 3, 5, 2, 1, 1, 2, 7, 7, 5, 1, 2, 1, 5,
-                 1, 7, 3, 4, 2, 4, 2, 3, 4, 3, 2, 1, 4, 7, 5, 4, 1, 5, 5, 2, 6, 1, 2, 6, 1, 7, 7, 5, 7, 4, 7, 7, 1, 3,
-                 4, 2, 2, 6, 1]
+                [1, 1, 4, 3, 3, 3, 4, 4, 4, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 5, 5,
+                 5, 5, 1, 1, 5, 3, 4, 4, 4, 4, 4, 0, 2, 3, 4, 0, 0, 0, 4, 0, 5,
+                 5, 0, 0, 5, 5, 1, 0, 3, 3, 1, 1, 2, 2, 5, 2, 3, 1, 1, 4, 6, 4,
+                 0, 2, 0, 0, 3, 6, 3, 1, 1, 5, 3, 3, 2, 1, 2, 1, 3, 3, 2, 4, 4,
+                 4, 1, 5, 4, 2, 4, 2, 2, 0, 0, 0, 2, 0, 0, 0, 3, 5, 1, 6, 2, 0,
+                 5, 3, 4, 1, 1, 4, 4, 0, 4, 2, 0, 4, 4, 0, 0, 2, 4, 5, 2, 2, 0,
+                 2, 1, 1, 4, 3, 5, 6, 3, 1, 2, 5, 2, 2, 5, 5, 5, 4, 5, 0, 5, 0,
+                 0, 0, 1, 0, 5, 5, 1, 0, 2, 6, 5, 1, 1, 5, 5, 3, 1, 4, 3, 1, 0,
+                 1, 1, 3, 0, 0, 5, 0, 0, 3, 0, 0, 4, 0, 3, 3, 3, 3, 0, 3, 3, 1,
+                 3, 0, 2, 4, 1, 0, 0, 1, 6, 6, 4, 0, 1, 0, 4, 0, 6, 2, 3, 1, 3,
+                 1, 2, 3, 2, 1, 0, 3, 6, 4, 3, 0, 4, 4, 1, 5, 0, 1, 5, 0, 6, 6,
+                 4, 6, 3, 6, 6, 0, 2, 3, 1, 1, 5, 0]
         self.action_lookup_table = action_lookup_table
 
     def set_lookup_table(self, action_lookup_table):
@@ -23,5 +28,5 @@ class LookupTableRobot(BaseRobot):
     def choose_action(self):
         self.sense_environment()
         self.calculate_situation_number()
-        action_number = self.action_lookup_table[self.situation_number] - 1  # - 1 because actions are zero based
+        action_number = self.action_lookup_table[self.situation_number]
         return self.actions[action_number]
